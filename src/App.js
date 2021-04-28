eb9cc990f426f544973c12f1279c4c6eec32ec4e
Marco Savastio <marco.savastio@qmi.it>
2021-04-28 11:55:21 +0200

    Created App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Cards from './components/Cards/Cards';
 import Viewer from './components/Viewer/Viewer';
 
 function App() {
   return (
     <div className="App">
       <Viewer />
+      <Cards />
     </div>
   );
 }
